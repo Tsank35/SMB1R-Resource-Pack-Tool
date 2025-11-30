0a8b893e8a6b7cb901a6b9ac689e29a9c72d4260
Tsank35 <tsankar1226@gmail.com>
2025-11-29 22:15:49 -0700

    Added error source

--- Scripts/Components/Variation/VariationBranch.gd
+++ Scripts/Components/Variation/VariationBranch.gd
@@ -119,7 +119,7 @@ func apply_json(json: Dictionary) -> void:
 	
 	for key in json.keys():
 		if key is String:
-			add_variation(key, Global.get_value_of_type(json, key, TYPE_DICTIONARY))
+			add_variation(key, Global.get_value_of_type(json, key, TYPE_DICTIONARY, self))
 		else:
 			MessageLog.type_error(TYPE_STRING, typeof(key), self)
 	apply_config = false
